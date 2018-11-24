d7997830c79746d4a19426726b2461e910d02914
Nico Klaassen <klaassen.nico@gmail.com>
2018-11-24 14:36:24 +0100

    Entity ENUM and expansion on Cell class

--- grid.py
+++ grid.py
@@ -1,21 +1,34 @@
 from hlt.positionals import Position
+from enum import Enum
+
+
+class Entity(Enum):
+    NONE = 0
+    FRIEND = 1
+    ENEMY = 2
+    SHIPYARD = 3
+    # Enemy shipyard?
+    # Additional dropoffs
 
 
 class Grid(object):
-    def __init__(self, game_map):
-        self.game_map = game_map
+    def __init__(self, game_map, player):
+        self._game_map = game_map
+        self._player = player
+
         self.grid = None
         self.max_w = 0
 
-        self.build_grid()
+        self.build_grid()    # Initialization for the cells and self.max_w
+        # self.connect_grid()  # Sets up the neighbour relations within each cell
 
     def build_grid(self):
         grid_cells = []
-        for row in range(self.game_map.height):
+        for row in range(self._game_map.height):
             grid_row = []
-            for col in range(self.game_map.width):
-                game_cell = self.game_map[Position(row, col)]
-                cell = Cell(row, col, game_cell.halite_amount)
+            for col in range(self._game_map.width):
+                game_cell = self._game_map[Position(row, col)]
+                cell = Cell(game_cell, self._player)
                 grid_row.append(cell)
 
                 # Now that we are looping over the grid, we immediately also calculate max_w for efficiency reasons
@@ -25,29 +38,35 @@ class Grid(object):
 
         self.grid = grid_cells
 
-        # # Test if an enemy is already on this resource
-        # resources = 0
-        # if cell.is_occupied:
-        #     logging.debug(f"SHIP DATA -------------------------------------- {cell.ship.owner} - {me.id}")
-        #     if cell.ship.owner != me.id or Position(row, col) == me.shipyard.position:
-        #         cell.mark_unsafe(cell.ship)
-        #     else:
-        #         resources = cell.halite_amount
-        #         if resources > resource_max:
-        #             resource_max = resources
-        # else:
-        #     resources = cell.halite_amount
-        #     if resources > resource_max:
-        #         resource_max = resources
-        # grid_row.append(resources)
-
-        # resource_map.append(row_resources)
-
 
 class Cell(object):
-    def __init__(self, x, y, w):
-        self.x = x
-        self.y = y
-        self.w = w
+    def __init__(self, game_cell, player): #x, y, w):
+        self._game_cell = game_cell
+        self._player = player
 
-        self.pos = Position(self.x, self.y)
+        self.pos = self._game_cell.position #Position(self.x, self.y)
+        self.x = self.pos.x
+        self.y = self.pos.y
+
+        self.w = self._game_cell.halite_amount
+
+        self.entity = self.get_entity()
+
+    def get_entity(self):
+        if self._game_cell.is_empty:
+            return Entity.NONE
+
+        elif self._game_cell.is_occupied:
+            ship = self._game_cell.ship
+            if ship.owner == self._player:
+                return Entity.FRIEND
+            else:
+                return Entity.ENEMY
+
+        else: # Shipyard (later on also dropoffs)
+            if self._game_cell.structure.owner == self._player:
+                return Entity.SHIPYARD
+            else:
+                return Entity.ENEMY
+
+
